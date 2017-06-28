538b912a22cf67acee43ebff79696767c853a437
Brian Cappello <briancappello@gmail.com>
2017-06-28 07:16:37 -0400

    install django rest framework

--- backend/settings.py
+++ backend/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_extensions',
+    'rest_framework',
 
     'app',
 ]
