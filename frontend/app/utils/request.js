474d9f8d17c1dd06526ce0f57fb71b1ce00c60a8
Brian Cappello <briancappello@gmail.com>
2017-06-30 13:48:35 -0400

    add convenience get/post functions to request util

--- frontend/app/utils/request.js
+++ frontend/app/utils/request.js
@@ -1,4 +1,5 @@
 import 'whatwg-fetch';
+import * as Cookies from 'js-cookie'
 
 /**
  * Parses the JSON returned by a network request
@@ -8,6 +9,11 @@ import 'whatwg-fetch';
  * @return {object}          The parsed JSON from the request
  */
 function parseJSON(response) {
+  // early return if no content
+  if (response.status === 204) {
+    return null
+  }
+
   return response.json();
 }
 
@@ -36,8 +42,60 @@ function checkStatus(response) {
  *
  * @return {object}           The response data
  */
-export default function request(url, options) {
+export function request(url, options) {
   return fetch(url, options)
     .then(checkStatus)
     .then(parseJSON);
 }
+
+export function get(url, options = {}) {
+  const _options = Object.assign({}, {
+    headers: {
+      'Accept': 'application/json',
+    },
+    method: 'GET',
+  }, options)
+  return request(url, _options)
+}
+
+export function post(url, data, options = {}) {
+  const _options = Object.assign({}, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFToken': Cookies.get('csrftoken'),
+    },
+    method: 'POST',
+    body: JSON.stringify(data),
+  }, options)
+  return request(url, _options)
+}
+
+export function authedGet(token, url, options = {}) {
+  const _options = Object.assign({}, {
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Token ${token}`,
+    },
+    method: 'GET',
+  }, options)
+  return request(url, _options)
+}
+
+export function authedPost(token, url, data = null, options = {}) {
+  const _options = Object.assign({}, {
+    credentials: 'include',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Token ${token}`,
+      'Content-Type': 'application/json',
+      'X-CSRFToken': Cookies.get('csrftoken'),
+    },
+    method: 'POST',
+    body: JSON.stringify(data),
+  }, options)
+  return request(url, _options)
+}
+
+export default request
