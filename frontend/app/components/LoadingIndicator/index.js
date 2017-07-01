92ad7eea2abc9a259211fea12c4f47f124a071f2
Brian Cappello <briancappello@gmail.com>
2017-07-01 14:54:08 -0400

    refactor LoadingIndicator to pass spinner color as classname

--- frontend/app/components/LoadingIndicator/index.js
+++ frontend/app/components/LoadingIndicator/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 import './loadingIndicator.scss'
 
-const LoadingIndicator = () => (
-  <div className="spinner-loading-indicator">
-    <div className="spinner-circle r0" />
-    <div className="spinner-circle r30" />
-    <div className="spinner-circle r60" />
-    <div className="spinner-circle r90" />
-    <div className="spinner-circle r120" />
-    <div className="spinner-circle r150" />
-    <div className="spinner-circle r180" />
-    <div className="spinner-circle r210" />
-    <div className="spinner-circle r240" />
-    <div className="spinner-circle r270" />
-    <div className="spinner-circle r300" />
-    <div className="spinner-circle r330" />
-  </div>
-);
+const LoadingIndicator = (props) => {
+  const { color } = props
+  return (
+    <div className={`spinner-loading-indicator ${color}`}>
+      <div className="spinner-circle r0" />
+      <div className="spinner-circle r30" />
+      <div className="spinner-circle r60" />
+      <div className="spinner-circle r90" />
+      <div className="spinner-circle r120" />
+      <div className="spinner-circle r150" />
+      <div className="spinner-circle r180" />
+      <div className="spinner-circle r210" />
+      <div className="spinner-circle r240" />
+      <div className="spinner-circle r270" />
+      <div className="spinner-circle r300" />
+      <div className="spinner-circle r330" />
+    </div>
+  )
+}
 
-export default LoadingIndicator;
+LoadingIndicator.defaultProps = {
+  color: 'black',
+}
+
+LoadingIndicator.propTypes = {
+  color: PropTypes.string.isRequired,
+}
+
+export default LoadingIndicator
