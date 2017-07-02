4efb1725c813ccb5b5a5b42d5d1a4f9daa21f30d
Brian Cappello <briancappello@gmail.com>
2017-07-02 10:09:07 -0400

    map dispatch to props by default in connected components

--- frontend/app/utils/index.js
+++ frontend/app/utils/index.js
@@ -1,11 +1,12 @@
 import { connect as reduxConnect } from 'react-redux'
 
-const emptyObjectFn = () => ({})
+const defaultMapStateToProps = () => ({})
+const defaultMapDispatchToProps = (dispatch) => ({ dispatch })
 
 export const connect = (component, mergeProps, options) => {
   return reduxConnect(
-    component.mapStateToProps || emptyObjectFn,
-    component.mapDispatchToProps || emptyObjectFn,
+    component.mapStateToProps || defaultMapStateToProps,
+    component.mapDispatchToProps || defaultMapDispatchToProps,
     mergeProps,
     options
   )(component)
